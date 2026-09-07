218331c6a1881556c382b4525eac08e4059ed64b
Christian Monsalve <chrismon850@gmail.com>
2026-09-06 20:27:58 -0400

    test case and output tabs created

--- features/practice/problem-page/code-editor/components/problem-test-cases-panel.tsx
+++ features/practice/problem-page/code-editor/components/problem-test-cases-panel.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { ProblemTestCaseDetail } from "@/features/practice/problem-page/code-editor/components/problem-test-case-detail";
 import { ProblemTestCaseTab } from "@/features/practice/problem-page/code-editor/components/problem-test-case-tab";
 import { testCases } from "@/features/practice/problem-page/code-editor/constants/problem-test-cases-data";
+import { SquareCheck, Terminal } from "lucide-react";
 
 export function ProblemTestCasesPanel() {
   const [selectedCaseId, setSelectedCaseId] = useState(testCases[0].id);
@@ -11,11 +12,12 @@ export function ProblemTestCasesPanel() {
 
   return (
     <div className="flex h-full flex-col overflow-hidden bg-background shadow-sm">
+      <div className="group flex border-b border-border p-1 items-center">
+        <button className="flex text-foreground hover:bg-accent py-1 px-2 gap-1 text-sm items-center rounded-sm"><SquareCheck className="text-emerald-400 p-0.5" /> Testcase</button>
+        <div className="h-4 border-r border-border transition-opacity duration-150 group-hover:opacity-0" />
+        <button className="flex text-foreground hover:bg-accent py-1 px-2 gap-1 text-sm items-center rounded-sm"><Terminal className="text-emerald-400 p-0.5" /> Output</button>
+      </div>
       <div className="border-b border-border px-3 py-2.5 space-y-2">
-        <h3 className="text-xs font-semibold uppercase tracking-[0.16em] text-foreground">
-          Test Session
-        </h3>
-
         <div className=" overflow-x-auto pb-1">
           <div className="flex min-w-max gap-1.5">
             {testCases.map((testCase) => {
